5e9e693241523b3d1e6a70c62f977d7457228430
Noah Grant <noahdgrant@gmail.com>
2023-11-08 22:11:50 -0500

    added observer location, satellite TLE api, and satellite tracking calculations

---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+# Title: ESE EP5 Satellite Tracker
+# Author: Noah Grant
+# Date: November 8, 2023
+
+from datetime import datetime
+import ephem
+import geocoder
+import requests
+
+TLE_URL = "http://tle.ivanstanojevic.me/api/tle/"
+ELEVATION_URL = "https://api.open-elevation.com/api/v1/lookup?locations="
+
+SATELLITE_NUMBER = 25544  # ISS
+DURATION_HOURS = 5
+
+
+def main():
+    print("ESE EP5 Satellite Tracker\n")
+
+    # Current latitude, longitude, and altitude
+    location = geocoder.ip("me")
+    elevation = requests.get(
+            f"{ELEVATION_URL}{location.latlng[0]},{location.latlng[1]}"
+            ).json()
+    meters = elevation["results"][0]["elevation"]
+
+    print("OBSERVER INFORMATION:")
+    print(f"Latitude: {location.latlng[0]}")
+    print(f"Longitude: {location.latlng[1]}")
+    print(f"Elevation: {meters}m")
+
+    # TLE info for satellite
+    tle = requests.get(f"{TLE_URL}{SATELLITE_NUMBER}").json()
+    satellite_name = tle["name"]
+    satellite_line1 = tle["line1"]
+    satellite_line2 = tle["line2"]
+
+    print("\nSATELLITE INFORMATION:")
+    print(f"Name: {satellite_name}")
+    print(f"TLE line 1: {satellite_line1}")
+    print(f"TLE line 2: {satellite_line2}")
+
+    # Calculate satellite position
+    observer = ephem.Observer()
+    observer.lon, observer.lat = location.latlng
+    observer.date = datetime.utcnow()
+
+    print("\nSATELLITE POSITION:")
+
+    for _ in range(DURATION_HOURS):
+        satellite = ephem.readtle(satellite_name, satellite_line1,
+                                  satellite_line2)
+
+        satellite.compute(observer)
+
+        print(f"Time: {observer.date} - Satellite: {satellite_name} - "
+              f"Azimuth: {satellite.az} - Altitude: {satellite.alt}")
+
+        observer.date += ephem.hour
+
+
+if __name__ == "__main__":
+    main()
