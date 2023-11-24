8b28f557b9f4c835baae63100b21a5f0d49f241c
Sorrybro467 <selmanbursal0@gmail.com>
2023-11-24 13:40:22 -0500

    updated testing code

--- tests/test_magnetometer.py
+++ tests/test_magnetometer.py
@@ -8,6 +8,8 @@ sys.path.append("..")
 from time import sleep
 import board
 from src.magnetometer import Magnetometer
+from src.encoder import Encoder
+from src.stepper import Stepper
 from smbus2 import SMBus
 import busio
 
@@ -18,11 +20,34 @@ def main():
         i2c_address = 0x1E
         magnetometer = Magnetometer(bus, i2c_address)
 
+        # Initialize encoder
+        bus = SMBus(11)
+        i2c_address_encoder = 0x36
+        encoder = Encoder(bus, i2c_address_encoder)
+
+        i2c_address_stepper = 0x0F
+        stepper = Stepper(bus, i2c_address_stepper)
+
+        # Calibration
+        encoder.calibrate_zero_degree()
+        angle = encoder.get_adjusted_angle()
+        print("Encoder angle: %d", angle)
+
         print("Displaying readings from magnetometer")
+
+        # Test loop
+        stepper.set_target_velocity(100000000)
         while True:
+            angle = encoder.get_adjusted_angle()
             reading = magnetometer.get_heading()
-            print(reading)
-            sleep(1)
+            if 0 <= angle < 359.9:
+                print("Encoder angle: ", angle)
+                print("magnometer is reading: ", reading)
+            else:
+                break
+
+        print("Loop finished...")
+        stepper.de_energize()
 
     except KeyboardInterrupt:
         print("Quitting...")
