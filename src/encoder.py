a286c36e5d79321bd86ab8c6881f6f32dc05757d
Noah Grant <105687410+noahdgrant@users.noreply.github.com>
2023-11-25 10:27:53 -0500

    I2C MUX (#4)
    Co-authored-by: WyattjkRichard <wyatt@rhouse.me>

--- src/encoder.py
+++ src/encoder.py
@@ -7,21 +7,25 @@ from smbus2 import SMBus
 import time
 
 class Encoder:
-    def __init__(self, bus, i2c_address):
-        self.AS5600_ADDRESS = i2c_address
+    def __init__(self, bus, channel):
+        self.AS5600_ADDRESS = 0x36
+        self.MUX_ADDRESS = 0x70
         self.ANGLE_REGISTER_HIGH = 0x0E
         self.ANGLE_REGISTER_LOW = 0x0F
         self.bus = bus
         self.zero_deg_raw_value = None
+        self.channel = channel
 
     def read_angle(self):
+        self.enable_channel()
         high_byte = self.bus.read_byte_data(self.AS5600_ADDRESS, self.ANGLE_REGISTER_HIGH)
         low_byte = self.bus.read_byte_data(self.AS5600_ADDRESS, self.ANGLE_REGISTER_LOW)
         angle_raw = (high_byte << 8) | low_byte
-        angle_deg = (angle_raw * 360) / 4096 #2^12 = 4096 this is a 12 bit res 
+        angle_deg = (angle_raw * 360) / 4096 # 2^12 = 4096 this is a 12 bit res
         return angle_deg
 
     def calibrate_zero_degree(self):
+        self.enable_channel()
         self.zero_deg_raw_value = (self.bus.read_byte_data(self.AS5600_ADDRESS, self.ANGLE_REGISTER_HIGH) << 8) | self.bus.read_byte_data(self.AS5600_ADDRESS, self.ANGLE_REGISTER_LOW)
 
     def get_adjusted_angle(self):
@@ -30,6 +34,10 @@ class Encoder:
             return None
 
         angle = self.read_angle()
-        adjusted_angle = angle - ((self.zero_deg_raw_value * 360)) / 4096 #this line need to be changed to fit our purpose 
+        adjusted_angle = angle - ((self.zero_deg_raw_value * 360)) / 4096 # this line need to be changed to fit our purpose
         adjusted_angle %= 360
         return adjusted_angle
+
+    def enable_channel(self):
+        self.bus.write_byte(self.MUX_ADDRESS, 1 << self.channel)
+        time.sleep(0.1)
