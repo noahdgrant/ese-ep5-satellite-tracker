40fda8d607b7f88997b9d0741b334d5259a35c8b
Noah Grant <noahdgrant@gmail.com>
2023-11-27 19:04:09 -0500

    testing magnetometer

--- tests/test_magnetometer.py
+++ tests/test_magnetometer.py
@@ -8,26 +8,64 @@ sys.path.append("..")
 from smbus2 import SMBus
 from src.magnetometer import Magnetometer
 from time import sleep
+import math
+
+REG_WHO_AM_I = 0x0F
+REG_CTRL_REG1 = 0x20
+REG_CTRL_REG2 = 0x21
+REG_CTRL_REG3 = 0x22
+REG_CTRL_REG4 = 0x23
+REG_CTRL_REG5 = 0x24
+REG_STATUS_REG = 0x27
+REG_OUT_X_L = 0x28
+REG_OUT_X_H = 0x29
+REG_OUT_Y_L = 0x2A
+REG_OUT_Y_H = 0x2B
+REG_OUT_Z_L = 0x2C
+REG_OUT_Z_H = 0x2D
+REG_TEMP_OUT_L = 0x2E
+REG_TEMP_OUT_H = 0x2F
+REG_INT_CFG = 0x30
+REG_INT_SRC = 0x31
+REG_INT_THS_L = 0x32
+REG_INT_THS_H = 0x33
 
 
 def main():
-    try:
-        bus = SMBus(1)
-        i2c_address = 0x1E
-        magnetometer = Magnetometer(bus, i2c_address)
+    # LIS3MDL Registers
+    # Initialize I2C bus
+    bus = SMBus(11)
+    address = 0x1E
 
-        print("Displaying readings from magnetometer")
+    # Initialize LIS3MDL
+    bus.write_byte_data(address, REG_CTRL_REG1, 0x70)  # Set sample rate to 80Hz
+    bus.write_byte_data(address, REG_CTRL_REG2, 0x00)  # Set scale to 4 gauss
+    bus.write_byte_data(address, REG_CTRL_REG3, 0x00)  # Set continuous conversion mode
+    bus.write_byte_data(address, REG_CTRL_REG1, 0x60)  # Set X and Y to ultra high performance mode
+    bus.write_byte_data(address, REG_CTRL_REG4, 0x0C)  # Set Z operative mode to high-performance mode
 
-        # Test loop
-        while True:
-            reading = magnetometer.read_mag()
-            print("magnometer is reading: ", reading)
-            sleep(1)
+    # Function to read sensor data
+    def read_sensor(address, register):
+        lsb = bus.read_byte_data(address, register)
+        msb = bus.read_byte_data(address, register+1)
+        value = (msb << 8) + lsb
+        if value >= 32768:
+            value = value - 65536
+        return value
 
-        print("Loop finished...")
+    while True:
+        # Read sensor data
+        x = read_sensor(address, REG_OUT_X_L)
+        y = read_sensor(address, REG_OUT_Y_L)
+        z = read_sensor(address, REG_OUT_Z_L)
 
-    except KeyboardInterrupt:
-        print("Quitting...")
+        # Calculate heading
+        heading = math.atan2(y, x)
+        if heading < 0:
+            heading += 2 * math.pi
+        heading_degrees = math.degrees(heading)
+
+        print("Magnetic North: ", heading_degrees)
 
 
 if __name__ == "__main__":
