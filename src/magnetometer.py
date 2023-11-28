1cfcf1184d173866e24cbb289bf431d42121240f
Noah Grant <noahdgrant@gmail.com>
2023-11-27 22:15:15 -0500

    magnetometer works enough to find North

--- src/magnetometer.py
+++ src/magnetometer.py
@@ -3,44 +3,102 @@
 # Date: November 23, 2023
 
 from math import atan2, degrees
+from time import sleep
+
+REG_WHO_AM_I = 0x0F
+REG_CTRL_REG1 = 0x20
+REG_CTRL_REG2 = 0x21
+REG_CTRL_REG3 = 0x22
+REG_CTRL_REG4 = 0x23
+REG_CTRL_REG5 = 0x24
+REG_STATUS_REG = 0x27
+REG_OUT_X_L = 0x28
+REG_OUT_X_H = 0x29
+REG_OUT_Y_L = 0x2A
+REG_OUT_Y_H = 0x2B
+REG_OUT_Z_L = 0x2C
+REG_OUT_Z_H = 0x2D
+REG_TEMP_OUT_L = 0x2E
+REG_TEMP_OUT_H = 0x2F
+REG_INT_CFG = 0x30
+REG_INT_SRC = 0x31
+REG_INT_THS_L = 0x32
+REG_INT_THS_H = 0x33
 
 
 class Magnetometer():
     def __init__(self, bus, i2c_address):
-        set_ccm = 0x22
-        self.x_msb = 0x29
-        self.x_lsb = 0x28
-        self.y_msb = 0x2B
-        self.y_lsb = 0x2A
-
         self.bus = bus
         self.i2c_address = i2c_address
 
-        self.bus.write_byte_data(self.i2c_address, set_ccm, 0)
+        # Set X and Y to ultra high-performance mode
+        self.bus.write_byte_data(self.i2c_address, REG_CTRL_REG1, 0x60)
+        # Set Z to ultra high-performance mode
+        self.bus.write_byte_data(self.i2c_address, REG_CTRL_REG4, 0x0C)
+        # Set sample rate to 155Hz
+        self.bus.write_byte_data(self.i2c_address, REG_CTRL_REG1, 0x02)
+        # Set scale to 4 gauss
+        self.bus.write_byte_data(self.i2c_address, REG_CTRL_REG2, 0x00)
+        # Set continuous conversion mode
+        self.bus.write_byte_data(self.i2c_address, REG_CTRL_REG3, 0x00)
 
-    # call repeatedly
-    def read_mag(self):
-        x_raw = self.bus.read_i2c_block_data(self.i2c_address, self.x_lsb, 2)
-        y_raw = self.bus.read_i2c_block_data(self.i2c_address, self.y_lsb, 2)
+        self.offset_x = -91.92
+        self.offset_y = -27.81
+        self.offset_z = 29.44
 
-        print(x_raw)
-        print(y_raw)
+        sleep(0.010)
 
-        x_h = x_raw[1] << 8
-        x_l = x_raw[0] & 0xFF00
-        x = x_l | x_h
+    def calibrate(self):
+        print("Calibrating magnetometer.")
+        print("Please rotate the sensor around all axes...")
+        x, y, z = self.get_reading()
+        min_x = max_x = x
+        min_y = max_y = y
+        min_z = max_z = z
 
-        y_h = y_raw[1] << 8
-        y_l = y_raw[0] & 0xFF00
-        y = y_l | y_h
+        for i in range(2000):
+            x, y, z = self.get_reading()
 
-        print(x)
-        print(y)
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+            min_z = min(min_z, z)
 
-        return self.reading_to_angle(x, y)
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+            max_z = max(max_z, z)
 
-    def reading_to_angle(self, x, y):
+            sleep(0.01)
+
+        # Calculate the middle of the min/max range
+        self.offset_x = (max_x + min_x) / 2
+        self.offset_y = (max_y + min_y) / 2
+        self.offset_z = (max_z + min_z) / 2
+
+        print("Calibration done")
+        print(f"x offset = {self.offset_x:6.2f}, " +
+              f"y offset = {self.offset_y:6.2f}, " +
+              f"z offset = {self.offset_z:6.2f}")
+
+    def get_reading(self):
+        data = self.bus.read_i2c_block_data(self.address, REG_OUT_X_L, 6)
+        x = data[0] | (data[1] << 8)
+        y = data[2] | (data[3] << 8)
+        z = data[4] | (data[5] << 8)
+
+        x = (x / 6842) * 100
+        y = (y / 6842) * 100
+        z = (z / 6842) * 100
+
+        return (x, y, z)
+
+    def vector_to_degrees(self, x, y):
         angle = degrees(atan2(y, x))
         if angle < 0:
             angle += 360
         return angle
+
+    def get_heading(self):
+        x, y, _ = self.get_reading()
+        x -= self.offset_x
+        y -= self.offset_y
+        return self.vector_to_degrees(x, y)
--- tests/test_magnetometer.py
+++ tests/test_magnetometer.py
@@ -7,109 +7,18 @@ sys.path.append("..")
 
 from smbus2 import SMBus
 from src.magnetometer import Magnetometer
-from time import sleep
-import math
-
-REG_WHO_AM_I = 0x0F
-REG_CTRL_REG1 = 0x20
-REG_CTRL_REG2 = 0x21
-REG_CTRL_REG3 = 0x22
-REG_CTRL_REG4 = 0x23
-REG_CTRL_REG5 = 0x24
-REG_STATUS_REG = 0x27
-REG_OUT_X_L = 0x28
-REG_OUT_X_H = 0x29
-REG_OUT_Y_L = 0x2A
-REG_OUT_Y_H = 0x2B
-REG_OUT_Z_L = 0x2C
-REG_OUT_Z_H = 0x2D
-REG_TEMP_OUT_L = 0x2E
-REG_TEMP_OUT_H = 0x2F
-REG_INT_CFG = 0x30
-REG_INT_SRC = 0x31
-REG_INT_THS_L = 0x32
-REG_INT_THS_H = 0x33
-
-offset_x = 0
-offset_y = 0
-offset_z = 0
 
 
 def main():
-    # LIS3MDL Registers
-    # Initialize I2C bus
     bus = SMBus(1)
-    address = 0x1E
+    i2c_address = 0x1E
+    magnetometer = Magnetometer(bus, i2c_address)
 
-    # Initialize LIS3MDL
-    bus.write_byte_data(address, REG_CTRL_REG1, 0x60)  # Set X and Y to ultra high-performance mode
-    bus.write_byte_data(address, REG_CTRL_REG4, 0x0C)  # Set Z to ultra high-performance mode
-    bus.write_byte_data(address, REG_CTRL_REG1, 0x02)  # Set sample rate to 155Hz
-    bus.write_byte_data(address, REG_CTRL_REG2, 0x00)  # Set scale to 4 gauss
-    bus.write_byte_data(address, REG_CTRL_REG3, 0x00)  # Set continuous conversion mode
-
-    sleep(0.010)
-
-    def calibrate():
-        print("Calibrating magnetometer. Please rotate the sensor around all axes...")
-        x, y, z = get_reading()
-        min_x = max_x = x
-        min_y = max_y = y
-        min_z = max_z = z
-
-        for i in range(2000):
-            x, y, z = get_reading()
-
-            min_x = min(min_x, x)
-            min_y = min(min_y, y)
-            min_z = min(min_z, z)
-
-            max_x = max(max_x, x)
-            max_y = max(max_y, y)
-            max_z = max(max_z, z)
-
-            sleep(0.01)
-
-        # Calculate the middle of the min/max range
-        offset_x = (max_x + min_x) / 2 / 100
-        offset_y = (max_y + min_y) / 2 / 100
-        offset_z = (max_z + min_z) / 2 / 100
-
-        print("Calibration done")
-        print(f"x offset = {offset_x}, y offset = {offset_y}, z offset = {offset_z}")
-
-    # Function to read sensor data
-    def get_reading():
-        data = bus.read_i2c_block_data(address, REG_OUT_X_L, 6)
-        x = data[0] | (data[1] << 8)
-        y = data[2] | (data[3] << 8)
-        z = data[4] | (data[5] << 8)
-
-        if x > 32767:
-            x -= 65536
-        if y > 32767:
-            y -= 65536
-        if z > 32767:
-            z -= 65536
-
-        return (x, y, z)
-
-    def vector_to_degrees(x, y):
-        angle = math.degrees(math.atan2(y, x))
-        if angle < 0:
-            angle += 360
-        return angle
-
-    def get_heading():
-        x, y, _ = get_reading()
-        x -= offset_x
-        y -= offset_y
-        return vector_to_degrees(x, y)
-
-    calibrate()
+    if input("Do you want to calibrate the magnetometer? [y/n]") == "y":
+        magnetometer.calibrate()
 
     while True:
-        print("Heading: {:.2f} degrees".format(get_heading()))
+        print("Heading: {:.2f} degrees".format(magnetometer.get_heading()))
 
 
 if __name__ == "__main__":
